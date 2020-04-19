922651a6d72dcb3f535be2d3d1ad4b8b967fc302
Dev-Nebe <cp.nebe@gmail.com>
2020-04-19 20:49:22 +0100

    chore: Initial app setup

---
+++ src/accounts/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from accounts.models import *
+
+
+def home(request):
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    context = {
+        'orders': orders,
+        'customers': customers
+    }
+
+    return render(request, 'accounts/dashboard.html', context)
+
+
+def products(request):
+    products = Product.objects.all()
+
+    return render(request, 'accounts/products.html', {'products': products})
+
+
+def customer(request):
+    return render(request, 'accounts/customer.html')
+# Create your views here.
